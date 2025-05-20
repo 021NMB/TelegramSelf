0973131150a93d6f52d7fbe38e43f695ebd4f137
021NMB <148716609+021NMB@users.noreply.github.com>
2025-05-21 00:38:32 +0330

    Update Information.py

--- lib/Information.py
+++ lib/Information.py
@@ -1,10 +1,10 @@
 from .library import *
 
-admin_user_id = 123456789 #<--- آیدی ادمین
-api_id = 123456789 #<--- آی پی آی آیدی
-api_hash = 'abcdefghigklmnopqrstuvwxyz' #<--- ای پی آی هش
-helper_username = 'BotUserName' #<--- یوزر ربات هلپر بدون @
-bot_token = 'abcdefghigklmnopqrstuvwxyz:466546' #<--- توکن ربات هلپر
+admin_user_id = 7607683883  #<--- آیدی ادمین
+api_id = 27678952 #<--- آی پی آی آیدی
+api_hash = '3b41840360dee828e05d2aa0a6dec56b' #<--- ای پی آی هش
+helper_username = 'SinaSelfhelperbot' #<--- یوزر ربات هلپر بدون @
+bot_token = '7632888509:AAHgLEk-gqDsifDlblmPkNtXF--CGMjH6Rk' #<--- توکن ربات هلپر
 
 client_id = '01e7dc6b41c3471b94efe87abeb05919'
 client_secret = '4f5f93af1ced4b0d9ba8440606803639'
